eae4e0030fb54e081e31cc184d8e33360470c986
Ben Schwartz <bemasc@google.com>
2018-10-15 13:41:36 -0400

    Count download bytes and early reset events
    This should make it easier to monitor and improve the reliability
    of the network subsystem.

---
+++ Android/app/src/main/java/app/intra/socks/ReliabilityMonitor.java
@@ -0,0 +1,117 @@
+package app.intra.socks;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.SystemClock;
+import app.intra.util.Names;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.analytics.FirebaseAnalytics.Param;
+import java.net.Socket;
+import sockslib.server.io.Pipe;
+import sockslib.server.io.PipeListener;
+import sockslib.server.io.SocketPipe;
+import sockslib.server.listener.PipeInitializer;
+
+/**
+ * This is a pipe initializer that monitors post-connection socket reliability.
+ */
+public class ReliabilityMonitor implements PipeInitializer {
+  private final Context context;
+
+  ReliabilityMonitor(Context context) {
+    this.context = context;
+  }
+
+  @Override
+  public Pipe initialize(Pipe pipe) {
+    pipe.addPipeListener(new ReliabilityListener(context));
+    return pipe;
+  }
+
+  /**
+   * This class converts pipe lifecycle callbacks into network reliability monitoring events.
+   */
+  private static class ReliabilityListener implements PipeListener {
+    private static final int HTTPS_PORT = 443;
+    private static final String RESET_MESSAGE = "Connection reset";
+    private final Context context;
+    private int downloadBytes = 0;
+    private int uploadBytes = 0;
+    private int uploadCount = 0;
+    private long startTime = -1;
+
+    ReliabilityListener(Context context) {
+      this.context = context;
+    }
+
+    private static int getPort(Pipe pipe) {
+      final Socket remoteSocket;
+      if (SocketPipe.INPUT_PIPE_NAME.equals(pipe.getName())) {
+        remoteSocket = (Socket) pipe.getAttribute(SocketPipe.ATTR_SOURCE_SOCKET);
+      } else if (SocketPipe.OUTPUT_PIPE_NAME.equals(pipe.getName())){
+        remoteSocket = (Socket) pipe.getAttribute(SocketPipe.ATTR_DESTINATION_SOCKET);
+      } else {
+        remoteSocket = null;
+      }
+      if (remoteSocket == null) {
+        return -1;
+      }
+      return remoteSocket.getPort();
+    }
+
+    @Override
+    public void onStart(Pipe pipe) {
+      startTime = SystemClock.elapsedRealtime();
+    }
+
+    @Override
+    public void onStop(Pipe pipe) {
+      FirebaseAnalytics analytics = FirebaseAnalytics.getInstance(context);
+
+      Bundle event = new Bundle();
+      event.putInt(Param.VALUE, uploadBytes + downloadBytes);
+
+      int port = getPort(pipe);
+      if (port >= 0) {
+        event.putInt(Names.PORT.name(), port);
+      }
+
+      if (startTime >= 0) {
+        // Socket connection duration is in seconds.
+        long duration = (SystemClock.elapsedRealtime() - startTime) / 1000;
+        event.putInt(Names.DURATION.name(), (int)duration);
+      }
+
+      analytics.logEvent(Names.BYTES.name(), event);
+    }
+
+    @Override
+    public void onTransfer(Pipe pipe, byte[] buffer, int bufferLength) {
+      if (SocketPipe.INPUT_PIPE_NAME.equals(pipe.getName())) {
+        downloadBytes += bufferLength;
+      } else {
+        uploadBytes += bufferLength;
+        ++uploadCount;
+      }
+    }
+
+    @Override
+    public void onError(Pipe pipe, Exception exception) {
+      if (!RESET_MESSAGE.equals(exception.getMessage())) {
+        return;
+      }
+      if (downloadBytes > 0) {
+        // Ignore reset events after data is flowing bidirectionally.
+        return;
+      }
+      if (getPort(pipe) != HTTPS_PORT) {
+        // Ignore non-HTTPS events.
+        return;
+      }
+      Bundle event = new Bundle();
+      event.putInt(Names.BYTES.name(), uploadBytes);
+      event.putInt(Names.CHUNKS.name(), uploadCount);
+      FirebaseAnalytics.getInstance(context).logEvent(Names.EARLY_RESET.name(), event);
+    }
+  }
+}
