6c2e5ba33d06f4a8942bdead55e7f7f799338f52
Ben Schwartz <bemasc@google.com>
2020-05-01 14:28:51 -0400

    Update IP script to Python3
    Also includes updated IP databases for May 2020

--- scripts/dbip_shrink.py
+++ scripts/dbip_shrink.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import csv
 import gzip
 import ipaddress
@@ -11,18 +12,17 @@ Writes a packed binary file suitable for bisection search.
 Thanks to DB-IP.com for offering a suitable database under a CC-BY license.
 """
 
-infile = gzip.open(sys.argv[1])
+infile = gzip.open(sys.argv[1], mode='rt')
 out_prefix = sys.argv[2]
 
 v4file = open(out_prefix + '.v4', 'wb')
 v6file = open(out_prefix + '.v6', 'wb')
 
-r = csv.reader(infile)
-for start, end, country in r:
-  a = ipaddress.ip_address(unicode(start))
+for start, end, country in csv.reader(infile):
+  a = ipaddress.ip_address(start)
   f = v4file if a.version == 4 else v6file
   f.write(a.packed)
-  f.write(country)
+  f.write(bytes(country, 'us-ascii'))
 
 infile.close()
 v4file.close()
