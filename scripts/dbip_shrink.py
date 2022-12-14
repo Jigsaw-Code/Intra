5d825ecad033331da86b7de6c2a1efa7a6b35aa6
Reza Rohani <theonlykingpin@outlook.com>
2022-12-14 17:48:43 +0330

    Update dbip_shrink.py
    In python It's more recommended that open files with a context manager so in cases of any unexpected error, context manager can handle situation and close the files properly. it is also close the file or files automatically after completed operation. and also cleaner and more readable code were provided.

--- scripts/dbip_shrink.py
+++ scripts/dbip_shrink.py
@@ -12,19 +12,14 @@ Writes a packed binary file suitable for bisection search.
 Thanks to DB-IP.com for offering a suitable database under a CC-BY license.
 """
 
-infile = gzip.open(sys.argv[1], mode='rt')
 out_prefix = sys.argv[2]
 
-v4file = open(out_prefix + '.v4', 'wb')
-v6file = open(out_prefix + '.v6', 'wb')
+with open(out_prefix + '.v4', 'wb') as v4file, \
+        open(out_prefix + '.v6', 'wb') as v6file, \
+        gzip.open(sys.argv[1], mode='rt') as infile:
 
-for start, end, country in csv.reader(infile):
-  a = ipaddress.ip_address(start)
-  f = v4file if a.version == 4 else v6file
-  f.write(a.packed)
-  f.write(bytes(country, 'us-ascii'))
-
-infile.close()
-v4file.close()
-v6file.close()
-
+    for start, end, country in csv.reader(infile):
+        addr = ipaddress.ip_address(start)
+        file = v4file if addr.version == 4 else v6file
+        file.write(addr.packed)
+        file.write(bytes(country, 'us-ascii'))
